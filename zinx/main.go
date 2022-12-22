bb4258a0bf47c14c0f6cf3ee1e79966ae985dc6f
wuyutaott <wuyutaoktm@163.com>
2022-12-22 08:21:46 +0800

    客户端发起一个连接成功

--- zinx/main.go
+++ zinx/main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net"
+	"time"
+)
 
 func main() {
-	fmt.Println("hello world")
+	listener, err := net.Listen("tcp4", "0.0.0.0:8888")
+	if err != nil {
+		fmt.Println("net.Listen error:", err)
+	}
+	defer listener.Close()
+
+	fmt.Println("监听端口成功")
+
+	if _, err := listener.Accept(); err != nil {
+		fmt.Println("accept error:", err)
+	}
+	fmt.Println("收到一个连接")
+
+	time.Sleep(3 * time.Second)
 }
